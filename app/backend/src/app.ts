c0266e677b8dbbb677fdc6d72d4969bbd0dff7a0
Paulo Henrique <ph46163835@gmail.com>
2022-10-25 14:27:30 -0300

    feat: add route login de user

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 
+import loginRoute from './database/router/login.router';
+
 class App {
   public app: express.Express;
 
@@ -10,6 +12,7 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use('/login', loginRoute);
   }
 
   private config():void {
