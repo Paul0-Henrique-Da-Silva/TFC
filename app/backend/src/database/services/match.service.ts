e6dff876a37757aed211dd69f7c4234f1aea217f
Paulo Henrique <ph46163835@gmail.com>
2022-10-26 17:18:53 -0300

    feat: services match

---
+++ app/backend/src/database/services/match.service.ts
@@ -0,0 +1,17 @@
+import IMatch from '../interfaces/IMatch';
+import MatchModel from '../models/MatchModel.ts';
+import Team from '../models/TeamsModel';
+
+export default class MatchService {
+  private modelMacth = MatchModel;
+  async getAll(): Promise<IMatch[]> {
+    const data = await this.modelMacth.findAll({
+      include: [{
+        model: Team, as: 'teamHome', attributes: ['teamName'],
+      }, {
+        model: Team, as: 'teamAway', attributes: ['teamName'],
+      }],
+    });
+    return data as IMatch[];
+  }
+}
