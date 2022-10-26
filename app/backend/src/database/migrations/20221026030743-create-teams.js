283b4ee4c2383c2e52893bc465c76322f15cceae
Paulo Henrique <ph46163835@gmail.com>
2022-10-26 14:55:50 -0300

    detalhe em migration

--- app/backend/src/database/migrations/20221026030743-create-teams.js
+++ app/backend/src/database/migrations/20221026030743-create-teams.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      teams: {
+      team_name: {
         type: Sequelize.STRING,
         allowNull: false,
       }
