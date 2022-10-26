f0d44ea1c233287b1deb18e30447fa9615eb74fd
Paulo Henrique <ph46163835@gmail.com>
2022-10-26 13:18:26 -0300

    feat: service team getById

--- app/backend/src/database/services/team.service.ts
+++ app/backend/src/database/services/team.service.ts
@@ -6,8 +6,12 @@ class TeamService {
 
   public async getAll(): Promise<ITeam[]> {
     const data = await this.modelTeam.findAll();
-
     return data;
+  }
+
+  public async getById(id: string): Promise<ITeam> {
+    const data = await this.modelTeam.findOne({ where: { id } });
+    return data as ITeam;
   }
 }
 
