8361f3d265254f6a9ddef95b4cfa062b1d54e1a4
Paulo Henrique <ph46163835@gmail.com>
2022-10-26 18:58:59 -0300

    feat: controller getProgressFilter

--- app/backend/src/database/controllers/match.controller.ts
+++ app/backend/src/database/controllers/match.controller.ts
@@ -6,8 +6,16 @@ class MatchController {
 
   public getAll = async (_request: Request, response: Response): Promise<Response> => {
     const data = await this.serviceMatch.getAll();
-
     return response.status(200).json(data);
+  };
+
+  public getMatchesByProgress = async (request: Request, response: Response): Promise<Response> => {
+    const { inProgress } = request.query;
+    if (inProgress !== 'true' && inProgress !== 'false') {
+      return this.getAll(request, response);
+    }
+    const filtered = await this.serviceMatch.getProgressFilter(inProgress === 'true');
+    return response.status(200).json(filtered);
   };
 }
 
