e0ebf39e27524d8c291a20a2bc5991dbc3cf7cb4
Paulo Henrique <ph46163835@gmail.com>
2022-10-27 17:04:36 -0300

    bug tets trybe

---
+++ app/backend/app/backend/src/app.ts
@@ -0,0 +1,43 @@
+import * as express from 'express';
+
+import loginRoute from './database/router/login.router';
+import teamRoute from './database/router/team.router';
+import macthRoute from './database/router/match.router';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+
+    // Não remover essa rota
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use('/login', loginRoute);
+    this.app.use('/login/validate', loginRoute);
+    this.app.use('/teams', teamRoute);
+    this.app.use('/matches', macthRoute);
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+// A execução dos testes de cobertura depende dessa exportação
+export const { app } = new App();
